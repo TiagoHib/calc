55d53dafa7c699fcf9b245a5c23b219ad04ce829
TiagoHib <107284709+TiagoHib@users.noreply.github.com>
2022-11-22 19:51:49 -0300

    Add files via upload

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,26 +1,62 @@
-function insert(num)
-{
+function insert(num){
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
-function clean()
-{
+
+function clean(){
     document.getElementById('resultado').innerHTML = "";
 }
-function back()
-{
+
+function back(){
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
 }
-function calcular()
-{
+
+function calcular(){
     var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado)
-    {
+    if(resultado){
         document.getElementById('resultado').innerHTML = eval(resultado);
     }
-    else
-    {
-        document.getElementById('resultado').innerHTML = "Nada..."
+    else{
+        document.getElementById('resultado').innerHTML = "Nada...";
+    }
+
+    var plays = document.getElementById('plays').innerHTML;   
+    if (plays > 1){
+        document.getElementById('plays').innerHTML = plays - 1;
+    }
+    else{
+        if(document.getElementById('resultado').innerHTML == 0){
+            var popup_win = document.getElementById('pop_win');
+            popup_win.classList.toggle("show");
+    
+            var elements = document.getElementsByClassName("botao");
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].disabled =true;
+            }
+        }
+        else{
+            var popup_lose = document.getElementById('pop_lose');
+            popup_lose.classList.toggle("show");
+            document.getElementById('plays').innerHTML = 0;
+
+            document.getElementById("zero").disabled = true;
+            var elements = document.getElementsByClassName("botao");
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].disabled =true;
+            }
+        }
+    }
+
+    if(document.getElementById('resultado').innerHTML == 0){
+        var popup_win = document.getElementById('pop_win');
+        popup_win.classList.toggle("show");
+
+        document.getElementById("zero").disabled = true;
+        var elements = document.getElementsByClassName("botao");
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].disabled =true;
+        }
     }
 }
+
